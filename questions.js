f22c593c5bb60bcb4413fad2e4639304009425b8
“noarrassam” <noor.s.rassam@gmail.com>
2022-02-23 14:02:36 -0500

    ‘addedAnswersArrayToInputsValues’

--- questions.js
+++ questions.js
@@ -1,13 +1,62 @@
 var questions = [
   {
-    title: "Commonly used data types DO NOT include:",
+    title: "Commonly used data types is JS:",
     choices: ["String, Boolean", "Number, Undefined", "Null", "All the above"],
-    answer: "alerts",
+    answer: "All the above",
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "alerts",
+    answer: "curly brackets",
+  },
+  {
+    title:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    choices: ["var", "let", "Both A and B", "None of the above"],
+    answer: "Both A and B",
+  },
+  {
+    title:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    choices: [
+      "getElementbyId()",
+      "getElementbyClassName()",
+      "Both A and B",
+      "None of the above",
+    ],
+    answer: "Both A and B",
+  },
+  {
+    title:
+      "Which of the following methods can be used to display data in some form using Javascript?",
+    choices: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above",
+    ],
+    answer: "All of the above",
+  },
+  {
+    title: "How can a datatype be declared to be a constant type?",
+    choices: ["const", "var", "let", "constant"],
+    answer: "const",
+  },
+  {
+    title:
+      "When the switch statement matches the expression with the given labels, how is the comparison done?",
+    choices: [
+      "Both the datatype and the result of the expression are compared",
+      "only the datatype of the expression is compared",
+      "only the value of the expression is compared",
+      "None of the above",
+    ],
+    answer: "Both the datatype and the result of the expression are compared",
+  },
+  {
+    title: "What keyword is used to check whether a given property is valid or not?",
+    choices: ["in", "is in", "exists", "lies"],
+    answer: "in",
   },
 ];
 
--- script4.js
+++ script4.js
@@ -7,7 +7,6 @@ var labels = rdo.getElementsByTagName("label");
 var arrays = [];
 var counter = 0;
 var valid = false;
-// var question;
 var key = 0;
 
 function allQuestions() {
@@ -23,17 +22,7 @@ function allQuestions() {
       console.log(key);
       // inputs[3].value = "correct";
       entry.innerHTML = questions[key].title;
-
-      for (let i = 0; i < inputs.length; i++) {
-        // if (inputs[i].value == 3) {
-        var strArr = [].slice.call(inputs);
-        console.log(strArr);
-        strArr.indexOf("input");
-        inputs[i].value = "correct";
-        console.log(inputs[i]);
-        // }
-      }
-
+      clearRdoButton();
       for (let arr in questions[key].choices) {
         arrays.push(questions[key].choices[arr]);
         console.log(arrays);
@@ -42,19 +31,28 @@ function allQuestions() {
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
         console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
       }
 
-      for (var j = 0; j < labels.length; j++) {
-        labels[j].innerHTML = arrays[j];
-        console.log(labels[j]);
-      }
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   if (labels[i] == "All the above") {
+      //     inputs[i].value = labels[i];
+      //   }
+      //   console.log(labels[j]);
+      // }
       break;
     case questions[1]:
       console.log(key);
-      inputs[3].value = "";
-      inputs[1].value = "correct";
-      entry.innerHTML = questions[key].title;
+      // inputs[3].value = "";
+      // inputs[1].value = "correct";
+      clearRdoButton();
       arrays = [];
+      entry.innerHTML = questions[key].title;
       for (let arr in questions[key].choices) {
         arrays.push(questions[key].choices[arr]);
         console.log(arrays);
@@ -63,15 +61,283 @@ function allQuestions() {
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
         console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
       }
 
-      for (var j = 0; j < labels.length; j++) {
-        labels[j].innerHTML = arrays[j];
-        console.log(labels[j]);
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i]);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[2]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
       }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[3]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
+      }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[4]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
+      }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[5]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
+      }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[5]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
+      }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[6]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
+      }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
+      break;
+    case questions[7]:
+      console.log(key);
+      // inputs[3].value = "";
+      // inputs[1].value = "";
+      // inputs[2].value = "correct";
+      clearRdoButton();
+      arrays = [];
+      entry.innerHTML = questions[key].title;
+      for (let arr in questions[key].choices) {
+        arrays.push(questions[key].choices[arr]);
+        console.log(arrays);
+      }
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].innerHTML = arrays[i];
+        console.log(inputs[i]);
+        labels[i].innerHTML = arrays[i];
+        console.log(labels[i]);
+        if (labels[i].innerHTML == questions[key].answer) {
+          inputs[i].value = questions[key].answer;
+        }
+      }
+      // for (var i = 0; i < inputs.length; i++) {
+      //   // inputs[i].innerHTML = arrays[i];
+      //   if (questions[key].answer) {
+      //     inputs[i].value = questions[key].answer;
+      //     console.log(inputs[i].value);
+      //   }
+      // }
+
+      // for (var j = 0; j < labels.length; j++) {
+      //   labels[j].innerHTML = arrays[j];
+      //   console.log(labels[j]);
+      // }
       break;
   }
   //});
+}
+
+function clearRdoButton() {
+  var ele = document.getElementsByName("rdos");
+  for (var i = 0; i < ele.length; i++) ele[i].checked = false;
 }
 
 function submitBtn() {
@@ -88,23 +354,24 @@ function submitBtn() {
   // button.addEventListener("click", allQuestions);
   button.addEventListener("click", function () {
     for (let i = 0; i < inputs.length; i++) {
-      if (inputs[i].value == "correct") {
+      if (inputs[i].value == labels[i].innerHTML) {
         if (inputs[i].checked) {
-          valid = true;
+          // valid = true;
+          alert("validation succussful");
+          key++;
+          allQuestions();
+          console.log(key);
+          counter++;
+          console.log(counter);
           break;
+        } else {
+          alert("please select");
+          key++;
+          allQuestions();
+          break;
+          // return false;
         }
       }
-    }
-    if (valid) {
-      alert("validation succussful");
-      key++;
-      allQuestions();
-      console.log(key);
-    } else {
-      alert("please select");
-      key++;
-      allQuestions();
-      return false;
     }
   });
 }
