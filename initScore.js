cd01646c67b0f80cf52081053e286120bd5fbc72
“noarrassam” <noor.s.rassam@gmail.com>
2022-02-27 23:14:11 -0500

    ‘addedLocalStorage’

---
+++ initScore.js
@@ -0,0 +1,8 @@
+let initScore = [
+  {
+    name: [],
+    score: [],
+  },
+];
+
+export { initScore };
--- script4.js
+++ script4.js
@@ -1,12 +1,124 @@
 import { questions } from "./questions.js";
+import { initScore } from "./initScore.js";
 
 var entry = document.getElementById("questionParagraph");
 var rdo = document.getElementById("questionDiv");
 var inputs = rdo.getElementsByTagName("input");
 var labels = rdo.getElementsByTagName("label");
+var seconds = document.getElementById("demo");
+var inputElements = document.querySelectorAll("input");
+var labelElements = document.querySelectorAll("label");
+
 var arrays = [];
 var counter = 0;
 var key = 0;
+var count = 15;
+var x;
+
+function interval() {
+  count -= 1;
+  if (count > 10) {
+    allQuestions();
+  } else {
+    clearInterval(x);
+    var i;
+
+    for (i = 0; i < inputElements.length; i++) {
+      if (inputElements.length > 0) {
+        inputElements[i].remove();
+        // labelElements[i].remove();
+      }
+    }
+
+    var btnID = document.getElementById("submitBtn");
+    btnID.remove();
+
+    document.querySelector("#questionDiv").children[0].innerHTML =
+      "Times up, Please Fill up the Below:";
+
+    let textFieldInit = document.createElement("input");
+    textFieldInit.type = "text";
+    textFieldInit.id = "initials";
+
+    document.querySelector("#questionDiv").children[1].innerHTML =
+      "Enter your Init";
+
+    document.querySelector("#questionDiv").children[3].innerHTML = "";
+
+    document
+      .querySelector("#questionDiv")
+      .children[3].appendChild(textFieldInit);
+
+    document.querySelector("#questionDiv").children[5].innerHTML =
+      "Enter your Score";
+
+    let textFieldScore = document.createElement("input");
+    textFieldScore.type = "text";
+    textFieldScore.id = "scores";
+
+    document.querySelector("#questionDiv").children[7].innerHTML = "";
+
+    document
+      .querySelector("#questionDiv")
+      .children[7].appendChild(textFieldScore);
+
+    let br = document.createElement("br");
+    document.body.appendChild(br);
+
+    let submit = document.createElement("input");
+    submit.type = "submit";
+    submit.value = "Submit";
+    submit.id = "btnInsert";
+
+    document.body.appendChild(submit);
+
+    let br1 = document.createElement("br");
+    document.body.appendChild(br1);
+
+    let br2 = document.createElement("br");
+    document.body.appendChild(br2);
+
+    let output = document.createElement("fieldset");
+    output.id = "lsOutput";
+    document.body.appendChild(output);
+    console.log(output);
+    let xe = document.createElement("legend");
+    var t = document.createTextNode("Insert Data");
+    xe.appendChild(t);
+    document.getElementById("lsOutput").appendChild(xe);
+    let div = document.createElement("div");
+    div.id = "output";
+    document.getElementById("lsOutput").appendChild(div);
+
+    const initials = document.getElementById("initials");
+    const scores = document.getElementById("scores");
+    const btnInsert = document.getElementById("btnInsert");
+    const AllOutput = document.getElementById("output");
+
+    btnInsert.onclick = function () {
+      const key = initials.value;
+      const value = scores.value;
+
+      if (key && value) {
+        localStorage.setItem(key, value);
+        location.reload();
+      }
+
+      console.log(key);
+      console.log(value);
+    };
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      let val = localStorage.getItem(key);
+      AllOutput.innerHTML += `${key}: ${val}<br />`;
+    }
+  }
+  seconds.innerHTML = count;
+}
+
+function counterTime() {
+  x = setInterval(interval, 1000);
+}
 
 function allQuestions() {
   let question = questions[key];
@@ -23,6 +135,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -43,6 +158,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -63,6 +181,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -83,6 +204,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -103,6 +227,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -123,6 +250,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -143,6 +273,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -163,6 +296,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -184,6 +320,9 @@ function allQuestions() {
 
       for (var i = 0; i < inputs.length; i++) {
         inputs[i].innerHTML = arrays[i];
+        inputs[i].id = "rd";
+        let id = document.getElementById("rd");
+        id.checked = true;
         console.log(inputs[i]);
         labels[i].innerHTML = arrays[i];
         console.log(labels[i]);
@@ -211,7 +350,7 @@ function submitBtn() {
     for (let i = 0; i < inputs.length; i++) {
       if (inputs[i].value == labels[i].innerHTML) {
         if (inputs[i].checked) {
-          alert("validation succussful");
+          alert("Correct");
           key++;
           allQuestions();
           console.log(key);
@@ -219,7 +358,7 @@ function submitBtn() {
           console.log(counter);
           break;
         } else {
-          alert("please select");
+          alert("Wrong Answer");
           key++;
           allQuestions();
           break;
@@ -229,5 +368,5 @@ function submitBtn() {
   });
 }
 
+counterTime();
 submitBtn();
-allQuestions();
